e0db231453992a3c9377fc8bc3e973159f7bc7e5
Bekah T <rayearth53@yahoo.com>
2022-12-04 21:23:58 -0600

    Update 1-1.py

--- Day1/1-1.py
+++ Day1/1-1.py
@@ -1,4 +1,28 @@
 def main():
-  count = 0
+  calorie_data = []
+
+  with open('input.txt') as file:
+    for line in file:
+      if line == "\n":
+        line = line.replace("\n", "space")
+        calorie_data.append(line)
+      else:
+        calorie_data.append(int(line.strip()))
+    calorie_data.append("space")
+  file.close()
+
+  sum_per_elf = 0
+  maximum_sum = 0
+
+  for item in calorie_data:
+    if item != "space":
+      sum_per_elf += item
+    elif item == "space":
+      if sum_per_elf > maximum_sum:
+        maximum_sum = sum_per_elf
+      sum_per_elf = 0
+
+  print(calorie_data)
+  print("Most calories from one elf: ", maximum_sum)
 
 main()
