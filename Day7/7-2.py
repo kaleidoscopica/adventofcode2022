8785cb4ebc79208fbcb554a47f0c836d5e5c8fa9
kaleidoscopica <rayearth53@yahoo.com>
2022-12-07 17:45:46 -0600

    7-2

---
+++ Day7/7-2.py
@@ -0,0 +1,31 @@
+def main():
+  stack = []
+  files = []
+  import itertools
+
+  with open('input.txt') as file:
+    data = file.read()
+  file.close()
+
+  for line in data.splitlines():
+    # if we're changing directories to go further up the tree, 
+    if line == '$ cd ..':
+      # pop the last element on the stack and return it as 'size',
+      size = stack.pop()
+      # append that size to the files list,
+      files.append(size)
+      # and add the size to that of the last element on the stack
+      stack[-1] += size
+    # else, if we're changing directories down into the tree, put an empty placeholder at the end of the stack; we'll add to this later
+    elif line.startswith('$ cd '):
+      stack.append(0)
+    # if the first char in the line is a digit, it's a file size - add its size to that of the last element on the stack
+    elif line[0].isdigit():
+      stack[-1] += int(line.split()[0])
+
+  # starting at the last element and going backward, accumulate the sizes and then extend the files list with them
+  files.extend(itertools.accumulate(stack[::-1]))
+
+  print(min(size for size in files if size >= max(files) - 40_000_000))
+
+main()
